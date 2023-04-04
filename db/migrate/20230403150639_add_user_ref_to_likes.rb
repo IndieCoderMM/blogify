b3cc1fb72f3a31dfea110028aaae694821786e1b
heinthant <hthant00chk@gmail.com>
2023-04-04 14:10:27 +0630

    Add references to users

--- db/migrate/20230403150639_add_user_ref_to_likes.rb
+++ db/migrate/20230403150639_add_user_ref_to_likes.rb
@@ -1,6 +1,5 @@
 class AddUserRefToLikes < ActiveRecord::Migration[7.0]
   def change
-    add_reference :likes, :user, null: false, foreign_key: true
-    rename_column :likes, :user_id, :author_id
+    add_reference :likes, :author, null: false, foreign_key: { to_table: :users}
   end
 end
--- db/migrate/20230403150658_add_user_ref_to_comments.rb
+++ db/migrate/20230403150658_add_user_ref_to_comments.rb
@@ -1,6 +1,5 @@
 class AddUserRefToComments < ActiveRecord::Migration[7.0]
   def change
-    add_reference :comments, :user, null: false, foreign_key: true
-    rename_column :comments, :user_id, :author_id
+    add_reference :comments, :author, null: false, foreign_key: { to_table: :users}
   end
 end
