8692b8c253339b65138ffa0baf83e18a5e0d288d
heinthant <hthant00chk@gmail.com>
2023-04-06 14:11:06 +0630

    Format code

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :likes_counter, numericality: { greater_than_or_equal_to: 0 }
-  validates :comments_counter, numericality: {greater_than_or_equal_to: 0}
+  validates :comments_counter, numericality: { greater_than_or_equal_to: 0 }
 
   after_save :update_counter
 
