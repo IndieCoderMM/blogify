319377397edc084e5127a2b26e3ffd9380d298d3
heinthant <hthant00chk@gmail.com>
2023-04-10 13:59:35 +0630

    Use resources for routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get "/users/:user_id/posts", to: "posts#index", as: "user_posts"
-  get "/users/:user_id/posts/:id", to: "posts#show", as: "user_post"
-  get "/users/:id", to: "users#show", as: "user"
-  get "/users", to: "users#index"
   root "users#index"
+  resources :users, only: [:index, :show] do 
+    resources :posts, only: [:index, :show]
+  end
 end
