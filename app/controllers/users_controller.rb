effb099a11a7724b658579731665c94cc6ef9366
heinthant <hthant00chk@gmail.com>
2023-04-10 14:06:16 +0630

    Add placeholder views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
-    def index 
-    end
+  def index
+    @users = User.all
+  end
 
-    def show 
-        @id = params[:id]
-    end
+  def show
+    @id = params[:id]
+  end
 end
