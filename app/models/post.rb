a646a745dae682ac439ae75d6b118d467cd84aa4
heinthant <hthant00chk@gmail.com>
2023-04-11 01:08:49 +0630

    Fix validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,8 @@ class Post < ApplicationRecord
   has_many :comments, inverse_of: 'post', dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true 
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
 
   after_save :update_counter
 
