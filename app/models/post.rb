c9577631dd4e24276423ba0c7d10f82e4849d335
heinthant <hthant00chk@gmail.com>
2023-04-11 01:11:49 +0630

    Remove whitespace

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   has_many :comments, inverse_of: 'post', dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true 
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
 
   after_save :update_counter
