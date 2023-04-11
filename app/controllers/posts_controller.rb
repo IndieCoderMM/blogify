f5ca24fa0eb2d9708dffd1634d45bf4aecff7b8e
heinthant <hthant00chk@gmail.com>
2023-04-11 21:38:31 +0630

    Add views and routes for post#new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,23 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def new 
+    @user = current_user
+    @post = Post.new 
+    # respond_to do |format|
+    #   format.html { render: :new, locals: { post: post }}
+  end
+
+  def create 
+    @post = Post.new(params.require(:post).permit(:title, :text))
+    @post.author = current_user
+    if @post.save
+      flash[:success] = "New post uploaded successfully!"
+      redirect_to user_post_url(current_user, @post)
+    else
+      flash.now[:error] = "Post upload failed!"
+      render :new 
+    end
+  end
 end
