2377b3c5d6dcda2d1938284fa6deb2d8cf3e8601
heinthant <hthant00chk@gmail.com>
2023-04-12 00:56:35 +0630

    Format code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,21 @@
 class CommentsController < ApplicationController
-    def new 
-        @user = current_user
-        @post = Post.find(params[:post_id])
-        @comment = Comment.new
-    end
+  def new
+    @user = current_user
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+  end
 
-    def create 
-        @post = Post.find(params[:post_id])
-        @comment = Comment.new(params.require(:comment).permit(:text))
-        @comment.author = current_user
-        @comment.post = @post
-        if @comment.save 
-            flash[:success] = "New comment added successfully!"
-            redirect_to user_post_url(@post.author, @post)
-        else
-            flash.now[:error] = "Comment upload failed!"
-            render "new"
-        end
-
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(params.require(:comment).permit(:text))
+    @comment.author = current_user
+    @comment.post = @post
+    if @comment.save
+      flash[:success] = 'New comment added successfully!'
+      redirect_to user_post_url(@post.author, @post)
+    else
+      flash[:error] = 'Comment upload failed!'
+      redirect_to new_user_post_comment_url(@post.author, @post)
     end
+  end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,19 +8,19 @@ class PostsController < ApplicationController
     @current = current_user
   end
 
-  def new 
+  def new
     @user = current_user
-    @post = Post.new 
+    @post = Post.new
   end
 
-  def create 
+  def create
     @post = Post.new(params.require(:post).permit(:title, :text))
     @post.author = current_user
     if @post.save
-      flash[:success] = "New post uploaded successfully!"
+      flash[:success] = 'New post uploaded successfully!'
       redirect_to user_post_url(current_user, @post)
     else
-      flash[:error] = "Post upload failed! Please try again."
+      flash[:error] = 'Post upload failed! Please try again.'
       redirect_to new_user_post_url(current_user)
     end
   end
