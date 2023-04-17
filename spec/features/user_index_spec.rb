90961fee8fc17af29d45ef1e2793e21683f012c3
heinthant <hthant00chk@gmail.com>
2023-04-17 15:36:43 +0630

    Add tests for user index

---
+++ spec/features/user_index_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe "User index page", type: :feature do 
+    let!(:user1) {User.create(name: "Tom", bio: "Engineer", photo: "photo1.jpg", posts_counter: 4)}
+    let!(:user2) {User.create(name: "Joe", bio: "Developer", photo: "photo2.jpg", posts_counter: 10)}
+
+    before do 
+        visit users_path 
+    end
+
+    describe "Page content" do 
+        it "should display all usernames" do 
+            expect(page).to have_content(user1.name)
+            expect(page).to have_content(user2.name)
+        end
+
+        it "should display all profile pictures" do 
+            expect(page).to have_css("img[src='#{user1.photo}']")
+            expect(page).to have_css("img[src='#{user2.photo}']")
+        end
+
+        it "should display number of posts for each user" do 
+            expect(page).to have_content("Posts: #{user1.posts_counter}")
+            expect(page).to have_content("Posts: #{user2.posts_counter}")
+        end
+    end
+
+    describe "Clicking on each user" do 
+        it "should redirect to user1's show page" do 
+            click_link user1.name 
+            expect(current_path).to eq user_path(user1)
+        end
+
+        it "should redirect to user2's show page" do 
+            click_link user2.name 
+            expect(current_path).to eq user_path(user2)
+        end
+    end
+
+end
+
