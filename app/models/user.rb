331d91f3cc1d6de4917919eed2ea0bcf61e2dd04
heinthant <hthant00chk@gmail.com>
2023-04-18 21:47:39 +0630

    Set up authentication
    Co-authored-by: tobuya <thomasobuya95@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+         
   has_many :posts, foreign_key: 'author_id', inverse_of: 'author', dependent: :destroy
   has_many :likes, foreign_key: 'author_id', inverse_of: 'author', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', inverse_of: 'author', dependent: :destroy
