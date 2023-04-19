1c90cdcd9535413920379440110537265491c5e6
heinthant <hthant00chk@gmail.com>
2023-04-20 02:00:38 +0630

    Set up comment#destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
+    @current_user = current_user
   end
 
   def show
