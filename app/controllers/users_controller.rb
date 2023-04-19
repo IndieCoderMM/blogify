c862048831b5770c70d7fc2e4f878ebaa55ebde7
heinthant <hthant00chk@gmail.com>
2023-04-20 01:07:24 +0630

    Add admin role to register form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @current_user = current_user
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,8 +7,10 @@ class Ability
     can :read, :all 
 
     return unless user.present?
+    can :manage, User, id: user.id 
     can :manage, Post, author_id: user.id
     can :manage, Comment, author_id: user.id
+    can :create, Like 
 
     return unless user.role == 'admin'
     can :manage, :all
